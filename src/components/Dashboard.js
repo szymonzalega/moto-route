5298cb5edbc7e9c40a42802a71fbc4c0b1762517
Załęga Szymon <Szymon.Zalega@asseco.pl>
2020-10-29 23:08:14 +0100

    Styles for Header

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,13 @@
-import React, { useState } from "react";
-import { Button, Alert } from "react-bootstrap";
-import { useAuth } from "../contexts/AuthContext";
-import { useHistory, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import RoutePage from "./routes/RoutePage";
 import UserProfile from "./UserProfile";
 import UpdateProfile from "./UpdateProfile";
 import ManageRoute from "./routes/ManageRoute";
-import * as userActions from "../redux/actions/userActions";
 import AddRoute from "./routes/AddRoute"
 
 export default function Dashboard() {
-  const [error, setError] = useState("");
-  const { logout } = useAuth();
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  async function handleLogout() {
-    setError("");
-
-    try {
-      dispatch(userActions.userLogout());
-      await logout();
-      history.push("/login");
-    } catch {
-      setError("Failed to log out");
-    }
-  }
 
   return (
     <>
@@ -43,14 +23,6 @@ export default function Dashboard() {
           component={UpdateProfile}
         />
       </Switch>
-
-      {error && <Alert variant="danger">{error}</Alert>}
-
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,88 @@
-import React from 'react'
-import {Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./Header.css";
+import { useDispatch } from "react-redux";
+import { useAuth } from "../contexts/AuthContext";
+import * as userActions from "../redux/actions/userActions";
+import HomeIcon from "@material-ui/icons/Home";
+import TerrainIcon from "@material-ui/icons/Terrain";
+import PersonIcon from "@material-ui/icons/Person";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { useHistory } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <nav>
-            <Link to="/index">Dashboard</Link>{" | "}
-            <Link to="/index/routes" >Routes</Link>{" | "}
-            <Link to="/index/user-profile" >Profile</Link>
-        </nav>
-    )
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { logout } = useAuth();
+  const [error, setError] = useState("");
+  const [activeView, setActiveView] = useState("dashboard");
+
+  function redirectToView(item) {
+    history.push(item.path);
+    setActiveView(item.id);
+  }
+
+  async function handleLogout() {
+    setError("");
+    try {
+      dispatch(userActions.userLogout());
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
+
+  let items = [
+    {
+      id: "dashboard",
+      path: "/index",
+      text: "Dashboard",
+      icon: <HomeIcon fontSize="large" />,
+    },
+    {
+      id: "routes",
+      path: "/index/routes",
+      text: "Routes",
+      icon: <TerrainIcon fontSize="large" />,
+    },
+    {
+      id: "user-profile",
+      path: "/index/user-profile",
+      text: "Profile",
+      icon: <PersonIcon fontSize="large" />,
+    },
+  ];
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        {items.map((item) => {
+          return (
+            <div
+              className={`header__element ${
+                activeView === item.id ? "header__element--active" : ""
+              }`}
+              onClick={() => redirectToView(item)}
+            >
+              {item.icon}
+              <span className="header__text">{item.text}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="header__right">
+        <div
+          className="header__element header__element--logout"
+          onClick={handleLogout}
+        >
+          <ExitToAppIcon fontSize="large" />
+          {error && (
+            <div className="header__logout-error">
+              <span>{error}</span>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
