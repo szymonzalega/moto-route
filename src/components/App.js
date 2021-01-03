a32888717f3da6768e79871dc3bd250f06d04e72
Załęga Szymon <Szymon.Zalega@asseco.pl>
2021-01-03 20:52:55 +0100

    Sidebar, App scss refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.scss"
 import Signup from "./auth/Signup";
 import { Container } from "react-bootstrap";
 import AuthProvider from "../contexts/AuthContext";
